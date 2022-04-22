a612dab40b49d069e63fa6643c9f8a2e0013b251
W4ited <510992113@qq.com>
2022-04-22 15:32:01 +0800

    RecommendFragment

--- app/src/main/java/com/coolweather/gofun/fragment/Recommandfragment.java
+++ app/src/main/java/com/coolweather/gofun/fragment/Recommandfragment.java
@@ -8,15 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.coolweather.gofun.R;
 
 public class Recommandfragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_recommand, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recommend_RecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
     }
 }
