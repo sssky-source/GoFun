f8a40f7cd18a69f8efc20aa3907922033c6fb2a8
W4ited <510992113@qq.com>
2022-04-28 11:48:39 +0800

    Retrofit2

---
+++ app/src/main/java/com/coolweather/gofun/util/HttpRequest.java
@@ -0,0 +1,27 @@
+package com.coolweather.gofun.util;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+
+* @Author : LWD
+
+* @Time : On 2022/4/28 9:41
+
+*/
+
+//请求封装
+public class HttpRequest {
+    //根地址
+    private static final String BASE_URI ="http://192.168.0.11:8080/";
+
+    private static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URI)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public static <T> T create(Class<T> serviceClass){
+        return retrofit.create(serviceClass);
+    }
+}
---
+++ app/src/main/java/com/coolweather/gofun/util/Service.java
@@ -0,0 +1,24 @@
+package com.coolweather.gofun.util;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+
+* @Author : LWD
+
+* @Time : On 2022/4/28 9:41
+
+*/
+
+//定义相对路径和方法
+public interface Service {
+    /*
+    * 实例
+    * @GET("Good/getGoodsByType?page=1&size=6&type=1")
+    * Call<List<Good>> getGoodData();
+    * */
+
+}
