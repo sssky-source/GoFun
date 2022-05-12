1f5a95b359a953636b90e4f299e4767a0b128d84
sky01 <1658554314@qq.com>
2022-05-12 10:50:13 +0800

    地图上的卡片组件

--- app/src/main/java/com/coolweather/gofun/activity/DetailInfoActivity.java
+++ app/src/main/java/com/coolweather/gofun/activity/DetailInfoActivity.java
@@ -3,7 +3,9 @@ package com.coolweather.gofun.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.coolweather.gofun.GoFunApplication;
 import com.coolweather.gofun.R;
 
 public class DetailInfoActivity extends AppCompatActivity {
@@ -12,5 +14,6 @@ public class DetailInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_info);
+        Log.d("TOK", GoFunApplication.getToken());
     }
 }
