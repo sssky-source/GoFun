9e44d5f2a69968a60a9b66a5bd85ddaaba771b05
帅哥 <1556802676@qq.com>
2023-02-28 18:05:52 +0800

    'feat:11'

--- app/src/main/java/com/coolweather/gofun/activity/DetailInfoActivity.java
+++ app/src/main/java/com/coolweather/gofun/activity/DetailInfoActivity.java
@@ -17,7 +17,7 @@ public class DetailInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_info);
-        Log.d("TOK", GoFunApplication.getToken());
+     //   Log.d("TOK", GoFunApplication.getToken());
     }
 
 }
